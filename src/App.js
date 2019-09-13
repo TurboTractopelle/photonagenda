e220508b6453b9ae8e8d2e3476903effefdac2f7
neroark <ns_ark@hotmail.com>
2019-09-13 14:50:18 +0200

    feat: set structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Agenda from "./containers/Agenda/Agenda";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Agenda />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/store/actions/actions.js
@@ -0,0 +1,4 @@
+import * as actionTypes from "./actionTypes";
+import actionCreator from "./actionCreator";
+
+export const setCat = actionCreator(actionTypes.SET_CAT, "cat");
